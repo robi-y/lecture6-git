b4f0a2d4f13f74b1a4cd514cfefeb97d6ee83eab
Reuven Yagel <robi@post.jce.ac.il>
2012-11-25 12:40:56 +0200

    luck 7

--- lottery.c
+++ lottery.c
@@ -10,18 +10,18 @@ int main(int argc, char** argv)
 {
     if (argc != 7)
     {
-        fprintf(stderr, "Usage: %s power_ball (5 white balls)\n", argv[0]);
+        fprintf(stderr, "Usage: %s (5 white balls) power_ball\n", argv[0]);
         return -1;
     }
 
-    int power_ball = atoi(argv[1]);
+    int power_ball = atoi(argv[6]);
 
     int white_balls[5];
     for (int i=0; i<5; i++)
     {
-        white_balls[i] = atoi(argv[2+i]);
+        white_balls[i] = atoi(argv[1+i]);
     }
-
+	
     int result = calculate_result(white_balls, power_ball);
 
     printf("%d percent chance of winning\n", result);
