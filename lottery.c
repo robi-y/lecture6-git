244c37ee85d7b156575d44d1f633f1c8d20de38d
jce-dev <robi.robi.y@gmail.com>
2012-11-25 12:34:59 +0200

    fix some warnings

--- lottery.c
+++ lottery.c
@@ -3,6 +3,25 @@
 
 int calculate_result(int white_balls[5], int power_ball)
 {
+    for (int i=0; i<5; i++)
+    {
+        if (
+             (white_balls[i] < 1)
+             || (white_balls[i] > 59)
+           )
+        {
+            return -1;
+        }
+    }
+
+    if (
+         (power_ball < 1)
+         || (power_ball > 39)
+       )
+    {
+        return -1;
+    }
+
     return 0;
 }
 
